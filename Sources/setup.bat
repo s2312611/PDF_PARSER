681cc50ffa98fe333ef189b6fdfc00ef98c3cd1e
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-12 18:08:59 +0900

    20260212 | Modified /Sources/setup.bat

--- Sources/setup.bat
+++ Sources/setup.bat
@@ -1,1 +1,7 @@
+@echo off
 python -m PyInstaller pypdfium2.py --onefile
+xcopy /Y /I "%~dp0dist\pypdfium2*" "..\Tests\"
+xcopy /Y /I "%~dp0dist\pypdfium2*" "..\"
+rmdir /S /Q "%~dp0build"
+rmdir /S /Q "%~dp0dist"
+del /Q "%~dp0pypdfium2.spec"
