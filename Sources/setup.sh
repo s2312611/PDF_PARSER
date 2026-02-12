9f92ed59387ed5ed4b5eece95a5caf982904084b
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-12 17:12:02 +0900

    20260212 | Modified /Sources/setup.sh

--- Sources/setup.sh
+++ Sources/setup.sh
@@ -1,3 +1,4 @@
 python -m PyInstaller pypdfium2.py --onefile
 cp ./dist/pypdfium2.exe ../Tests/pypdfium2.exe
+cp ./dist/pypdfium2.exe ../Programs/pypdfium2.exe
 rm ./build/ ./dist/ pypdfium2.spec --recursive
