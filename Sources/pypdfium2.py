31763d86f857559a31ffe5e0f9919573268a00d7
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-12 15:34:33 +0900

    20260212 | Modified /Sources/pypdfium2.py

--- Sources/pypdfium2.py
+++ Sources/pypdfium2.py
@@ -4,16 +4,16 @@ import pypdfium2 as pdfium
 def search_pdfs():
     for directorypath, _, filenames in os.walk("."):
         for filename in filenames:
-            if ospath.split(filename)[1].lower() == ".pdf":
+            if ospath.splitext(filename)[1].lower() == ".pdf":
                 yield ospath.join(directorypath, filename)
 
 def extract_text(path):
     pdf = pdfium.PdfDocument(path)
-    txt = f"{ospath.splitext(pdf)[0]}.txt"
+    txt = f"{ospath.splitext(path)[0]}.txt"
     pages = [page for page in pdf]
     textpages = [page.get_textpage() for page in pages]
     texts = [textpage.get_text_range() for textpage in textpages]
     text = "\n".join(texts)
-    with open(txt, "w") as path:
-        path.write(text)
+    with open(txt, "w") as txtfile:
+        txtfile.write(text)
     return text
