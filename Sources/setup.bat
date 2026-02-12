70e06c6cb4a84e8a0288aec1146418219d9bfedc
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-12 18:04:56 +0900

    20260212 | Modified /Sources/setup.bat

--- Sources/setup.bat
+++ Sources/setup.bat
@@ -1,6 +1,1 @@
-py -m pip install pypdfium2
-py -m pip install pyinstaller
-py -m PyInstaller pypdfium2.py --onefile
-cp ./dist/pypdfium2* ../Tests/
-cp ./dist/pypdfium2* ../
-rm ./build/ ./dist/ pypdfium2.spec --recursive
+python -m PyInstaller pypdfium2.py --onefile
