9d5f3fa6e424efc93bb321addc846717582cd3bb
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-12 20:37:39 +0900

    20260212 | Modified /Sources/setup.bat

--- Sources/setup.bat
+++ Sources/setup.bat
@@ -2,8 +2,8 @@
 pip install pypdfium2
 pip install pyinstaller
 pyinstaller pypdfium2.py --onefile
-xcopy /Y /I "%~dp0dist\pypdfium2*" "..\Tests\"
-xcopy /Y /I "%~dp0dist\pypdfium2*" "..\"
+xcopy /Y /I "%~dp0dist\basedon_pypdfium2*" "..\Tests\"
+xcopy /Y /I "%~dp0dist\basedon_pypdfium2*" "..\"
 rmdir /S /Q "%~dp0build"
 rmdir /S /Q "%~dp0dist"
-del /Q "%~dp0pypdfium2.spec"
+del /Q "%~dp0basedon_pypdfium2.spec"
