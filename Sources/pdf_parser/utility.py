ea9aabc4f3c288793a6f90709bd57bd0d08df400
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-13 22:55:53 +0900

    20260213 | Modified /Sources/pdf_parser/utility.py

--- Sources/pdf_parser/utility.py
+++ Sources/pdf_parser/utility.py
@@ -1,5 +1,15 @@
 import os, os.path as ospath
 
+def validate_existence(path):
+    if isinstance(path, str):
+        path = path.strip()
+    else:
+        raise Exception #!
+    if ospath.exists(path):
+        return path
+    else:
+        raise Exception #!
+
 def search_pdfs():
     for directorypath, _, filenames in os.walk("."):
         for filename in filenames:
