0e5962ed5f6a51879e0604a4f1ca963072aeed56
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-28 21:26:24 +0900

    20260228 | Deleted /Sources/setup.bat

--- Sources/setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pip install pypdfium2
-pip install pyinstaller
-pyinstaller pypdfium2_module.py --onefile
-xcopy /Y /I "%~dp0dist\pypdfium2_module*" "..\Tests\"
-xcopy /Y /I "%~dp0dist\pypdfium2_module*" "..\"
-rmdir /S /Q "%~dp0build"
-rmdir /S /Q "%~dp0dist"
-del /Q "%~dp0pypdfium2_module.spec"
