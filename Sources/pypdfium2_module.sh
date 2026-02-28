27ba11897a0951458ea27e9d293e4183691e1270
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-28 21:26:12 +0900

    20260228 | Modified /Sources/pypdfium2_module.sh

---
+++ Sources/pypdfium2_module.sh
@@ -0,0 +1,8 @@
+pip install pypdfium2
+pip install pyinstaller
+pyinstaller pypdfium2_module.py --onefile
+cp ./dist/pypdfium2_module* ../
+cp ./dist/pypdfium2_module* ../Tests/
+rm --recursive --force ./build
+rm --recursive --force ./dist
+rm ./pypdfium2_module.spec
