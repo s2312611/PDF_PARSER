b4238d9fc31535df2c5bef31474980d4d8f45078
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-02-28 21:31:18 +0900

    20260228 | Modified /Sources/pypdfium2_module.sh

--- Sources/pypdfium2_module.sh
+++ Sources/pypdfium2_module.sh
@@ -1,8 +1,8 @@
 pip install pypdfium2
 pip install pyinstaller
 pyinstaller pypdfium2_module.py --onefile
-cp ./dist/pypdfium2_module* ../
-cp ./dist/pypdfium2_module* ../Tests/
+cp ./dist/pypdfium2_module.exe ../
+cp ./dist/pypdfium2_module.exe ../Tests/
 rm --recursive --force ./build
 rm --recursive --force ./dist
 rm ./pypdfium2_module.spec
