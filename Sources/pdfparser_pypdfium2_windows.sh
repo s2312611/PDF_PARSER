3d54eb8e9ea4951c0e94821a00acb14068db74eb
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-03-02 23:58:34 +0900

    20260302 | Modified /Sources/pdfparser_pypdfium2_windows.sh

--- Sources/pdfparser_pypdfium2_windows.sh
+++ Sources/pdfparser_pypdfium2_windows.sh
@@ -3,13 +3,13 @@ name="${1:-pdfparser_pdfium2}"
 zipfile=./$name.zip
 targzfile=./$name.tar.gz
 target=./dist
-python -m venv ./venv/
+python -m venv ./venv
 source ./venv/Scripts/activate
 $execute_python -m pip install pip==26.0.0
 $execute_python -m pip install pypdfium2==5.5.0
 pyinstaller -Fn pdfparser.exe ./pdfparser_pypdfium2.py
 mkdir $target/licenses
 zip -r $zipfile $target
-tar -czf $targzfile -C $target ./
+tar -czf $targzfile -C $target .
 deactivate
-rm -rf ./venv/
+rm -rf ./venv
