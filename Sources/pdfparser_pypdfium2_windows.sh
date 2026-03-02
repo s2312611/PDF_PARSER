3c0797d154562bff0d956a2621640009c14440cb
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-03-02 21:00:29 +0900

    20260302 | Modified /Sources/pdfparser_pypdfium2_windows.sh

--- Sources/pdfparser_pypdfium2_windows.sh
+++ Sources/pdfparser_pypdfium2_windows.sh
@@ -1,7 +1,6 @@
 python -m venv ./venv/
 source ./venv/Scripts/activate
 execute_python="./venv/Scripts/python"
-modules="./venv/Lib/site-packages/"
 $execute_python -m pip install --upgrade pip==26.0.0
 $execute_python -m pip install -r ./requirements_pypdfium2.txt
 deactivate
