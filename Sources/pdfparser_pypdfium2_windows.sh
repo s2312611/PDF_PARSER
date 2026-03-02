4640106ea9f5db51a101b9ea2e78346eb1ca6431
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-03-02 23:41:33 +0900

    20260302 | Modified /Sources/pdfparser_pypdfium2_windows.sh

--- Sources/pdfparser_pypdfium2_windows.sh
+++ Sources/pdfparser_pypdfium2_windows.sh
@@ -3,7 +3,7 @@ source ./venv/Scripts/activate
 execute_python="./venv/Scripts/python"
 $execute_python -m pip install --upgrade pip==26.0.0
 $execute_python -m pip install -r ./requirements_pypdfium2.txt
-pyinstaller -F ./pdfparser_pypdfium2.py
+pyinstaller -Fn pdfparser.exe ./pdfparser_pypdfium2.py
 name="${1:-pdfparser_pdfium2}"
 zipfile=./$name.zip
 targzfile=./$name.tar.gz
