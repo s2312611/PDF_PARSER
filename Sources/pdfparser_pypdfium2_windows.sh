1f07b2ef96eb6105f1419067edb88ef38dc4b3d1
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-03-03 00:13:08 +0900

    20260303 | Modified /Sources/pdfparser_pypdfium2_windows.sh

--- Sources/pdfparser_pypdfium2_windows.sh
+++ Sources/pdfparser_pypdfium2_windows.sh
@@ -7,12 +7,12 @@ execute_python=./venv/Scripts/python
 $execute_python -m pip install pip==26.0.0
 packages=./venv/Lib/site-packages
 $execute_python -m pip install pypdfium2==5.5.0
-dependency01p=$(find $packages -type d -name pypdfium*info)
-dependency01l=$(find $dependency01p -type d -name LICENSES)
-dependency01d=$(find $dependency01p -type d -name BUILD_LICENSES)
 pyinstaller -Fn pdfparser.exe ./pdfparser_pypdfium2.py
 target=./dist
 licenses=$target/licenses
+dependency01p=$(find $packages -type d -name pypdfium*info)
+dependency01l=$(find $dependency01p -type d -name LICENSES)
+dependency01d=$(find $dependency01p -type d -name BUILD_LICENSES)
 mkdir $licenses
 zip -r $zipfile $target
 tar -czf $targzfile -C $target .
