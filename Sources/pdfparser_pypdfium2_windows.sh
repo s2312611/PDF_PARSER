64580bca5c50dc9a4fab2e644c7cd10644384df6
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-03-02 20:54:21 +0900

    20260302 | Modified /Sources/pdfparser_pypdfium2_windows.sh

--- Sources/pdfparser_pypdfium2_windows.sh
+++ Sources/pdfparser_pypdfium2_windows.sh
@@ -1,5 +1,6 @@
 python -m venv ./venv/
 source ./venv/Scripts/activate
+./venv/Scripts/python -m pip install --upgrade pip==26.0.0
 ./venv/Scripts/python -m pip install -r ./requirements_pypdfium2.txt
 deactivate
 rm --recursive --force ./venv/
