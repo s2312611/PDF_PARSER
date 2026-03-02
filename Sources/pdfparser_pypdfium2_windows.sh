585133bcc799b52f11fb90d76e954bde02e9c119
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-03-02 21:01:02 +0900

    20260302 | Modified /Sources/pdfparser_pypdfium2_windows.sh

--- Sources/pdfparser_pypdfium2_windows.sh
+++ Sources/pdfparser_pypdfium2_windows.sh
@@ -3,5 +3,6 @@ source ./venv/Scripts/activate
 execute_python="./venv/Scripts/python"
 $execute_python -m pip install --upgrade pip==26.0.0
 $execute_python -m pip install -r ./requirements_pypdfium2.txt
+pyinstaller --onefile ./pdfparser_pypdfium2.py
 deactivate
 rm --recursive --force ./venv/
