1dfb41a0347cecbac2636a81d8342e08073312f8
s2312611 <171270978+s2312611@users.noreply.github.com>
2026-03-03 04:33:44 +0900

    20260303 | Modified /Sources/pythonlicense.sh

--- Sources/pythonlicense.sh
+++ Sources/pythonlicense.sh
@@ -1,6 +1,11 @@
-sed -e 's/[[:space:]]//g' -e 's/\\/\\\\/g' ./venv/pyvenv.cfg > ./pyvenv.temp
-rm ./pyvenv.temp
-dependency00p=
-dependency00l=
+temp=./pyvenv.temp
+sed -e 's/[[:space:]]//g' -e 's/\\/\\\\/g' ./venv/pyvenv.cfg > $temp
+source $temp
+target=./dist
+dependency00p=$(cd $home && pwd)
+dependency00l=$dependency00p/LICENSE.txt
 dependency00d=
-dependency00t=
+dependency00t=./dist/Licenses/Python
+mkdir -p $dependency00t
+cp $dependency00l $dependency00t
+rm $temp
